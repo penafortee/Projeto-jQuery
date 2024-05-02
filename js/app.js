a2f648ecc6443a266308bc004b327804ad897296
Nathália Penaforte de Freitas <106880589+penafortee@users.noreply.github.com>
2024-05-02 15:46:19 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,42 +1,22 @@
-$(document).ready(function(){
+$('[data-group]').each(function(){
+    var $allTarget = $(this).find('[data-target]'),
+        $allClick = $(this).find('[data-click]'),
+        activeClass = 'active';
 
-    var classActive = 'active'
+    $allTarget.first().addClass(activeClass);
+    $allClick.first().addClass(activeClass);
 
-    // Ação para deixar o primeiro item já ativo no Menu.
-    $('.animais .tab-menu a').first().addClass(classActive);
-    $('.animais .item').first().addClass(classActive);
-    //
-
-    // Função para animação do Menu
-    $('.animais .tab-menu a').click(function(e){
+    $allClick.click(function(e){
         e.preventDefault();
-        var itemId = $(this).attr('href');
 
-        //Removemos o 'active' para troca de menu
-        $('.animais .tab-menu a').removeClass(classActive);
-        $('.animais .item').removeClass(classActive);
+        var id = $(this).data('click'),
+            $target = $('[data-target="' + id + '"]');
 
-        // adicionamos a classe conforme o click
-        $(this).addClass(classActive);
-        $(itemId).addClass(classActive);
-    });
+        $allClick.removeClass(activeClass);
+        $allTarget.removeClass(activeClass);
 
-    // Ação para deixar o primeiro item já ativo no Menu.
-    $('.florestas .tab-menu a').first().addClass(classActive);
-    $('.florestas .item').first().addClass(classActive);
-    //
+        $target.addClass(activeClass);
+        $(this).addClass(activeClass);
 
-    // Função para animação do Menu
-    $('.florestas .tab-menu a').click(function(e){
-        e.preventDefault();
-        var itemId = $(this).attr('href');
-
-        //Removemos o 'active' para troca de menu
-        $('.florestas .tab-menu a').removeClass(classActive);
-        $('.florestas .item').removeClass(classActive);
-
-        // adicionamos a classe conforme o click
-        $(this).addClass(classActive);
-        $(itemId).addClass(classActive);
     });
 });
