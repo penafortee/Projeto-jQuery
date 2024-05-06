51a9d3b71d775097a0ed40975d7188da59435e4b
Nathália Penaforte de Freitas <106880589+penafortee@users.noreply.github.com>
2024-05-06 16:57:36 -0300

    update 06-05

--- js/app.js
+++ js/app.js
@@ -20,3 +20,64 @@ $('[data-group]').each(function(){
 
     });
 });
+
+
+$('.menu-nav a[href^="#"]').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        menuHeight = $('.menu').innerHeight();
+        targetOffset = $(id).offset().top;
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);        
+});
+
+$('.logo').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500)
+});
+
+$('section').each(function(){
+    var height = $(this).height(),
+        offsetTop = $(this).offset().top,
+        menuHeight = $('.menu').innerHeight(),
+        id = $(this).attr('id'),
+        $itemMenu = $('a[href="#' + id +'"]');
+
+    $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+        if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+            $itemMenu.addClass('active');
+        } else {
+            $itemMenu.removeClass('active');
+        }
+    });
+
+});
+
+
+$('.mobile-btn').click(function() {
+    $(this).toggleClass('active');
+    $('.mobile-menu').toggleClass('active');
+});
+
+
+
+
+$('.slide > :first').addClass('active');
+
+function rotateSlide() {
+	var activeSlide = $('.slide > .active'),
+		nextSlide = activeSlide.next();
+	
+	if(nextSlide.length == 0) {
+		nextSlide = $('.slide > :first');
+	}
+	activeSlide.removeClass('active');
+	nextSlide.addClass('active');
+} 
+
+setInterval(rotateSlide, 3000);
